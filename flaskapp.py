9fa0b90585b10bfe621819984ff7ed94063afddf
Michael Castro <michael.castro@drake.edu>
2026-03-24 23:09:26 -0500

    Analyze word code

--- flaskapp.py
+++ flaskapp.py
@@ -29,7 +29,9 @@ def hello(name):
 #  Each exercise asks you to add a new @app.route here
 # ============================================================
 
-
+@app.route('/analyze/<word>')
+def analyze(word):
+    return str(len(word))
 
 
 # ============================================================
